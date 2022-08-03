b9cfd949737706f452920d138953cd02811d5080
blackpanther <vikasbhapri@gmail.com>
2022-08-03 21:25:21 +0530

    backend init

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/?directConnection=true&tls=false&readPreference=primary';
+
+const connectToMongo = async () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("connected to mongo")
+    })
+}
+
+module.exports = connectToMongo;
