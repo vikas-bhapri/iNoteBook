53f4ccda4bcf399408e971a7d172e6ef726fe6e1
blackpanther <vikasbhapri@gmail.com>
2022-08-06 00:45:50 +0530

    pre commit

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -8,7 +8,7 @@ const { body, validationResult } = require("express-validator");
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Notes.find({ user: req.user.id });
-    res.json([]);
+    res.json(notes);
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Internal server error");
@@ -46,5 +46,26 @@ router.post(
     }
   }
 );
+
+//Route 3: Update an existing node using PUT '/api/notes/updatenote'. Login required
+
+router.put('/updatenote:id', fetchuser, async (req,res) => {
+  const {title, description, tag} = req.body
+  const newNote = {}
+  if(title){newNote.title = title} 
+  if(description){newNote.description = description} 
+  if(tag){newNote.tag = tag} 
+
+  //find the note to be updated
+  let note = await Notes.findById(req.params.id)
+  if(!note){
+    return res.status(400).send("Not found!")
+  }
+  if(note.user.toString() !== req.user.id){
+    return res.status(503).send("Not allowed")
+  }
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+  res.json({note}) 
+})
 
 module.exports = router;
