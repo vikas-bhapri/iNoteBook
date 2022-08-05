c68611dadfc9b693e28c605499142fa697dd2dbd
blackpanther <vikasbhapri@gmail.com>
2022-08-06 00:50:15 +0530

    update notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,23 +49,25 @@ router.post(
 
 //Route 3: Update an existing node using PUT '/api/notes/updatenote'. Login required
 
-router.put('/updatenote:id', fetchuser, async (req,res) => {
-  const {title, description, tag} = req.body
-  const newNote = {}
-  if(title){newNote.title = title} 
-  if(description){newNote.description = description} 
-  if(tag){newNote.tag = tag} 
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+  const {title, description, tag} = req.body;
+  // Create a newNote object
+  const newNote  = {};
+  if(title){newNote.title = title};
+  if(description){newNote.description = description};
+  if(tag){newNote.tag = tag};
 
-  //find the note to be updated
-  let note = await Notes.findById(req.params.id)
-  if(!note){
-    return res.status(400).send("Not found!")
-  }
+  // Find the note to be updated and update it
+  let note = await Notes.findById(req.params.id);
+  if(!note){return res.status(404).send("Not Found")}
+
   if(note.user.toString() !== req.user.id){
-    return res.status(503).send("Not allowed")
+      return res.status(401).send("Not Allowed");
   }
-  note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
-  res.json({note}) 
-})
+
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+  res.json({note});
+  
+  })
 
 module.exports = router;
