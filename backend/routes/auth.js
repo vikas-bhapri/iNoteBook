00ffea40a1babb9244fba9d877528b45565cc857
blackpanther <vikasbhapri@gmail.com>
2022-08-05 20:52:22 +0530

    add notes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,28 +1,94 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser.js');
+const JWT_SECRET = 'aslfkjsdflkjsaf'
 
-
-// Create a user using POST "/api/auth". No auth required
-router.post('/', [
-    body('name').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password','Enter atleast 8 characters').isLength({min:8})
-],  (req,res)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+//Route 2: Create a user using POST "/api/auth/createuser". No auth required
+router.get('/createuser', [
+  body('name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Enter atleast 8 characters').isLength({ min: 8 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) { // Check for errors in user input 
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    let user = await User.findOne({ email: req.body.email }) // Check if the user already exists 
+  if (user) {
+    return res.status(400).json({ error: "Email already exists please login with this email" })
+  }
+  const salt = await bcrypt.genSalt(10)
+  const secPass = await bcrypt.hash(req.body.password, salt)
+  user = await User.create({ // Create a new user
+    name: req.body.name,
+    email: req.body.email,
+    password: secPass
+  })
+  const data = {
+    user:{
+      id: user.id
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      }).then(user => res.json(user)).catch(err=>{
-        console.log(err);
-        res.status(400).json({error:'Please enter valid credentials'})
-      })
+  }
+  const jwtData = jwt.sign(data, JWT_SECRET)
+  console.log(jwtData)
+  // res.json({ user })
+  res.send({jwtData})
+  } catch (error) {
+    res.status(500).json("Internal server error")
+  }
+})
 
+//Route 2: Create a user using POST "/api/auth/login". No auth required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Cannot be blank').exists()
+], async (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) { // Check for errors in user input 
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email}); // find user in the database
+    if(!user){
+      return res.status(400).json({error:"Enter correct credentials"});
+    }
+    const passwordCompare = await bcrypt.compare(password,user.password) // compare the hash
+    if(!passwordCompare){
+      return res.status(400).json({error:"Enter correct credentials"});
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+    const jwtData = jwt.sign(payload, JWT_SECRET); // jwt auth
+    res.json({jwtData})
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json("Internal server error")
+  }
+
+}
+)
+
+//Route 3: Get user details after login using POST : '/api/auth/getuser'. Login requried
+router.post('/getuser',fetchuser, async (req,res) => {
+try {
+  const userid = req.user.id;
+  const user = await User.findOne({userid}).select('-password')
+  res.send(user)
+} catch (error) {
+  console.log(error.message)
+  res.status(500).json("Internal server error")
+}
 })
 
 module.exports = router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser.js");
+const Notes = require("../models/Notes.js");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req,res)=>{
-    
-    res.json([])
-})
+//Route 1: Get all the notes of the user using GET 'api/notes/fetchallnotes'. Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json([]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
-module.exports = router
+//Route 2: Add a new note using POST 'api/notes/addnote'. Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Enter a valid description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const { title, description, tag } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Check for errors in user input
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+module.exports = router;
